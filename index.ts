97b93fe5eaae0849eb99c36866f341e6bccc6bd4
danh20051995 <danh.danh20051995@gmail.com>
2022-03-16 15:28:46 +0700

    remove: demo

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export * from './src/components/slider-3d'
 export * from './src/components/slider-3d/slide'
 export * from './src/components/slider-3d/controls'
 
-export * from './src/components/demo'
+// export * from './src/components/demo'
